dac7ab1bd02d1e382b95c828f9c8c48ee5751a3b
JuanAntonaccio <jmantonaccio@gmail.com>
2021-09-16 02:08:57 +0000

    no se que esta pasando

---
+++ src/js/component/semaforo.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const [prendo, setPrendo] = useState(0);
+
+const Semaforo = () => {
+	return (
+		<div className="container">
+			<h1>hola</h1>
+		</div>
+	);
+};
+
+export default Semaforo;
