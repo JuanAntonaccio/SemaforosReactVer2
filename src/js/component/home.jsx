23d48ff871eda0c11c92820c990cc43f5f085336
JuanAntonaccio <jmantonaccio@gmail.com>
2021-09-16 17:05:04 +0000

    se corrige el error, useState dentro del componente

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,30 +1,26 @@
 import React, { useState } from "react";
 
-const [prendo, setPrendo] = useState("red");
 //let prendo = "red";
 //utilice este home para encontrar el error
 const Home = () => {
+	const [prendo, setPrendo] = useState(0);
 	return (
 		<div className="container">
 			<div
-				onClick={() => setPrendo("red")}
+				onClick={() => setPrendo(1)}
+				className={"luz roja" + (prendo == 1 ? " brillo" : "")}></div>
+			<div
+				onClick={() => {
+					setPrendo(2);
+				}}
 				className={
-					"luz roja" + (prendo == "red" ? " brillo" : "")
+					"luz amarilla" + (prendo == 2 ? " brillo" : "")
 				}></div>
 			<div
-				//onClick={() => {
-				//	setPrendo("yellow");
-				//}}
-				className={
-					"luz amarilla" + (prendo == "yellow" ? " brillo" : "")
-				}></div>
-			<div
-				//onClick={() => {
-				//	setPrendo("green");
-				//}}
-				className={
-					"luz verde" + (prendo == "green" ? " brillo" : "")
-				}></div>
+				onClick={() => {
+					setPrendo(3);
+				}}
+				className={"luz verde" + (prendo == 3 ? " brillo" : "")}></div>
 		</div>
 	);
 };
